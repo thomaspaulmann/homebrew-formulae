71081bdf36f4d0795638784d35d2ba0b8ea00297
Thomas Paul Mann <hi@thomaspaulmann.com>
2016-11-08 19:47:43 +0100

    Fix SHA256 mismatch

--- Formula/swish.rb
+++ Formula/swish.rb
@@ -2,7 +2,7 @@ class Swish < Formula
   desc "Build your Swift projects on a remote machine."
   homepage "https://github.com/thomaspaulmann/Swish"
   url "https://github.com/thomaspaulmann/Swish/releases/download/1.5/swish.tar.gz"
-  sha256 "..."
+  sha256 "20f5ac8e92f36c8afeab75c1a126d8ed21ce1626dd9a2e163f7200ed532e99de"
 
   def install
     bin.install "swish"
