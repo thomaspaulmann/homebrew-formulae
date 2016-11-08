6a59dfc0276b624b13a5f6f37d6c17190de85633
Thomas Paul Mann <hi@thomaspaulmann.com>
2016-11-08 19:46:57 +0100

    Bump version to 1.5

--- Formula/swish.rb
+++ Formula/swish.rb
@@ -1,11 +1,11 @@
 class Swish < Formula
   desc "Build your Swift projects on a remote machine."
   homepage "https://github.com/thomaspaulmann/Swish"
-  url "https://github.com/thomaspaulmann/Swish/archive/1.3.tar.gz"
-  sha256 "4c07409b9c3b79b99cfa1b6a32e605af7ece43a48c8d251a60757c876c63924d"
+  url "https://github.com/thomaspaulmann/Swish/releases/download/1.5/swish.tar.gz"
+  sha256 "..."
 
   def install
-    bin.install "Scripts/swish"
+    bin.install "swish"
   end
 
   test do
